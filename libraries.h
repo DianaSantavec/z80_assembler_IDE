01cbb70ed061ce770af66936ed368ba53676a493
Diana <diana.santavec@gmail.com>
2019-10-09 12:03:56 +0200

    napravljen prozor i menubar i izmenjena komanda za kompajliranje

--- libraries.h
+++ libraries.h
@@ -6,42 +6,60 @@ void hello_world(GtkWidget *window, gpointer data){
     g_message ("hello world!\n");
 }
 
-GtkWidget create_window(){
-    GtkWidget *window;
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-//    gtk_widget_show(window);
-    g_signal_connect(window, "destroy",
-    G_CALLBACK(gtk_main_quit), NULL);  
-
-    return *window;
+void destroy(GtkWidget *widget, gpointer data){
+    gtk_main_quit();
 }
 
-    static GtkItemFactoryEntry menu_items[] = {
-        { "/_File",         NULL,         NULL, 0, "<Branch>" },
-        { "/File/_New",     "<control>N", hello_world, 0, NULL },
-        { "/File/_Open",    "<control>O", hello_world, 0, NULL },
-        { "/File/_Save",    "<control>S", hello_world, 0, NULL },
-        { "/File/Quit",     "<control>Q", gtk_main_quit, 0, NULL },
-        { "/_Edit",         NULL,         NULL, 0, "<Branch>" },
-        { "/Edit/copy",     "<control>C", hello_world, 0, NULL },
-        { "/Edit/cut",      "<control>X", hello_world, 0, NULL },
-        { "/Edit/paste",    "<control>V", hello_world, 0, NULL },
-        { "/_Help",         NULL,         NULL, 0, "<LastBranch>" },
-        { "/_Help/About",   NULL,         NULL, 0, NULL },
-    };
+static GtkItemFactoryEntry menu_items[] = {
+    { "/_File",         NULL,         NULL,          0, "<Branch>"     },
+    { "/File/_New",     "<control>N", hello_world,   0, NULL           },
+    { "/File/_Open",    "<control>O", hello_world,   0, NULL           },
+    { "/File/_Save",    "<control>S", hello_world,   0, NULL           },
+    { "/File/Quit",     "<control>Q", gtk_main_quit, 0, NULL           },
+    { "/_Edit",         NULL,         NULL,          0, "<Branch>"     },
+    { "/Edit/copy",     "<control>C", hello_world,   0, NULL           },
+    { "/Edit/cut",      "<control>X", hello_world,   0, NULL           },
+    { "/Edit/paste",    "<control>V", hello_world,   0, NULL           },
+    { "/_Help",         NULL,         NULL,          0, "<LastBranch>" },
+    { "/_Help/About",   "<control>H", hello_world,   0, NULL           },
+};
+GtkWidget *window;
+GtkWidget *vertikal_container_box;
+GtkWidget *menubar;
 
-void get_menu( GtkWidget  *window, GtkWidget **menubar ){
+void create_window(){
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_default_size(GTK_WINDOW(window), 500, 800); 
+  gtk_signal_connect (window, "destroy", G_CALLBACK(destroy), NULL);
+  
+  create_menubar();
+  
+  gtk_widget_show (menubar);
+  gtk_widget_show(window);
+}
+
+void create_menubar(){
+  vertikal_container_box = gtk_vbox_new (FALSE , 5);
+  gtk_container_border_width (GTK_CONTAINER (vertikal_container_box), 1);
+  gtk_container_add (GTK_CONTAINER(window), vertikal_container_box);
+  gtk_widget_show (vertikal_container_box);
+  
+  create_menu (window, &menubar);
+  gtk_box_pack_start (GTK_BOX (vertikal_container_box), menubar, FALSE, TRUE, 5);
+}
+
+void create_menu ( GtkWidget  *window, GtkWidget **menubar ){
   GtkItemFactory *item_factory;
-  GtkAccelGroup *accel_group;
+  GtkAccelGroup *keyboard_accelerators;
   gint nmenu_items = sizeof (menu_items) / sizeof (menu_items[0]);
 
-  accel_group = gtk_accel_group_new ();
+  keyboard_accelerators = gtk_accel_group_new ();
 
-  item_factory = gtk_item_factory_new (GTK_TYPE_MENU_BAR, "<main>", 
-                                       accel_group);
+  item_factory = gtk_item_factory_new (GTK_TYPE_MENU_BAR, "<main>", keyboard_accelerators);
   gtk_item_factory_create_items (item_factory, nmenu_items, menu_items, NULL);
-  gtk_window_add_accel_group (GTK_WINDOW (window), accel_group);
+  gtk_window_add_accel_group (GTK_WINDOW (window), keyboard_accelerators);
 
-  if (menubar)
+  if (menubar){
     *menubar = gtk_item_factory_get_widget (item_factory, "<main>");
+  }
 }
--- window.c
+++ window.c
@@ -1,20 +1,9 @@
 #include <libraries.h>
 
 int main(int argc, char *argv[]) {
-  GtkWidget *window, *menu, *main_vbox;
-
   gtk_init(&argc, &argv);
-  *window = create_window();
-  gtk_signal_connect (GTK_OBJECT(window), "destroy", GTK_SIGNAL_FUNC(gtk_main_quit),"WM destroy");
-  main_vbox = gtk_vbox_new(FALSE, 1);
-  gtk_container_border_width (GTK_CONTAINER (main_vbox), 1);
-  gtk_container_add (GTK_CONTAINER (window), main_vbox);
-  gtk_widget_show (main_vbox);
-  get_menu (window, &menu);
-  gtk_box_pack_start (GTK_BOX (main_vbox), menu, FALSE, TRUE, 0);
-  gtk_widget_show (menu);
 
-  gtk_widget_show(window);
+  create_window();
   gtk_main();
 
   return 0;
