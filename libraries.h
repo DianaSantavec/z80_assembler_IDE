4bf7567c580ae80d3674e2dcb40d6f25c13e63d3
Diana <diana.santavec@gmail.com>
2019-10-13 22:25:46 +0200

    SREDJENO - postavlja se text field i menu bar (ali radi GTK2), menu_bar nema funkcije

--- libraries.h
+++ libraries.h
@@ -27,33 +27,13 @@ static GtkItemFactoryEntry menu_items[] = {
 GtkWidget *window;
 GtkWidget *vertical_container_box;
 GtkWidget *menu_bar;
-GtkWidget *vertical_container_box_text;
 GtkWidget *text_view; 
 GtkTextBuffer *text_buffer;
-//GtkWidget *grid;
 
 
 GtkActionGroup *action_group;
 
 GtkBox *widget_box;
-//menu items
-/*
-GtkWidget *file_list;
-GtkWidget *edit_list;
-GtkWidget *help_list;
-
-GtkWidget *file_menu;
-GtkWidget *edit_menu;
-GtkWidget *help_menu;
-
-GtkWidget *new_item;
-GtkWidget *save_item;
-GtkWidget *open_item;
-GtkWidget *quit_item;
-GtkWidget *copy_item;
-GtkWidget *cut_item;
-GtkWidget *paste_item;
-GtkWidget *help_item;*/
 
 void on_open_file(){
     printf("hello world");
@@ -64,26 +44,15 @@ void create_window(){
   gtk_window_set_default_size(GTK_WINDOW(window), 500, 800); 
   gtk_window_set_title(GTK_WINDOW(window),"Z80 assembler IDE");
   g_signal_connect (window, "destroy", G_CALLBACK(destroy), NULL);
-  
- // grid = gtk_grid_new();
 
   create_menubar();
 
   create_textbox_filed();
-
-  //gtk_box_pack_start(GTK_BOX(widget_box),vertical_container_box,0,0,1);
-  //gtk_box_pack_start(GTK_BOX(widget_box),vertical_container_box_text,0,0,1);
-  
-  //gtk_widget_show (menu_bar);
-  gtk_widget_show (text_view);
-  //gtk_widget_show(window);
 }
 
 void create_menubar(){  
     vertical_container_box = gtk_vbox_new (FALSE , 5);
     gtk_container_border_width (GTK_CONTAINER (vertical_container_box), 1);
-    //gtk_container_add (GTK_CONTAINER(window), vertical_container_box);
-    //gtk_widget_show (vertical_container_box);
   
     create_menu (window, &menu_bar);
     gtk_box_pack_start (GTK_BOX (vertical_container_box), menu_bar, FALSE, TRUE, 5);
@@ -105,61 +74,7 @@ void create_menu ( GtkWidget  *window, GtkWidget **menubar ){
   }
 }
 
-/*void create_menubar(){
-   /* vertical_container_box = gtk_vbox_new(FALSE, 0);
-    gtk_container_add (GTK_CONTAINER (window), vertical_container_box);
-    menu_bar = gtk_menu_bar_new();
-    gtk_box_pack_start(GTK_BOX(vertical_container_box), menu_bar, FALSE, TRUE, 5);
-
-    file_list = gtk_menu_item_new_with_label("File");
-    edit_list = gtk_menu_item_new_with_label("Edit");
-    help_list = gtk_menu_item_new_with_label("Help");
-
-    gtk_menu_shell_append(GTK_MENU_SHELL (menu_bar), file_list);
-    gtk_menu_shell_append(GTK_MENU_SHELL (menu_bar), edit_list);
-    gtk_menu_shell_append(GTK_MENU_SHELL (menu_bar), help_list);
-
-    file_menu = gtk_menu_new();
-    edit_menu = gtk_menu_new();
-    help_menu = gtk_menu_new();
-
-    gtk_menu_item_set_submenu(GTK_MENU_ITEM(file_list), file_menu);
-    gtk_menu_item_set_submenu(GTK_MENU_ITEM(edit_list), edit_menu);
-    gtk_menu_item_set_submenu(GTK_MENU_ITEM(help_list), help_menu);
-
-    new_item = gtk_menu_item_new_with_label ("New");
-    save_item = gtk_menu_item_new_with_label ("Save");
-    open_item = gtk_menu_item_new_with_label ("Open");
-    quit_item = gtk_menu_item_new_with_label ("Quit");
-    copy_item = gtk_menu_item_new_with_label ("Copy");
-    cut_item = gtk_menu_item_new_with_label ("Cut");
-    paste_item = gtk_menu_item_new_with_label ("Paste");
-    help_item = gtk_menu_item_new_with_label ("Help");
-
-    gtk_menu_shell_append(GTK_MENU_SHELL(file_menu),new_item);
-    gtk_menu_shell_append(GTK_MENU_SHELL(file_menu),save_item);
-    gtk_menu_shell_append(GTK_MENU_SHELL(file_menu),open_item);
-    gtk_menu_shell_append(GTK_MENU_SHELL(file_menu),quit_item);
-    gtk_menu_shell_append(GTK_MENU_SHELL(edit_menu),copy_item);
-    gtk_menu_shell_append(GTK_MENU_SHELL(edit_menu),cut_item);
-    gtk_menu_shell_append(GTK_MENU_SHELL(edit_menu),paste_item);
-    gtk_menu_shell_append(GTK_MENU_SHELL(help_menu),help_item);
-
-    *//*
-
-    static const GtkActionEntry file_entries[] = {
-        {"FileMenu", NULL, "_File"},
-        {"Open", GTK_STOCK_OPEN, "_Open","<control>O","Open a file", G_CALLBACK(on_open_file)},
-    };
-
-    action_group = gtk_action_group_new("Common_Actions");
-    gtk_action_group_add_action(action_group,file_entries,G_N_ELEMENTS (file_entries),(gpointer)(&appState));
-
-}*/
-
 void create_textbox_filed(){
-    vertical_container_box_text = gtk_vbox_new(FALSE,2);
-    //gtk_container_add(GTK_CONTAINER(window),vertical_container_box_text);
 
     text_view = gtk_text_view_new();
     gtk_box_pack_start (GTK_BOX (vertical_container_box), text_view, 1, 1, 0);
